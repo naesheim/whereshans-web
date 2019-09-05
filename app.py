6e7b0e9e3a9d72f6b7bb029172f5b85db6387b66
naesheim <hg.nesheim@gmail.com>
2019-09-05 18:56:48 +0200

    add ci

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def base():
     return render_template('index.html', data=pos)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0')
