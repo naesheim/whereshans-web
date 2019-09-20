73777bebc687132d60b2f16d842f61d1b1bab82b
naesheim <hg.nesheim@gmail.com>
2019-09-20 21:55:52 +0200

    clean up code and bump requirements

--- app.py
+++ app.py
@@ -5,25 +5,20 @@ import os
 
 app = Flask(__name__, static_url_path='/static')
 
-API_ADDR = os.environ['API_URL']
-
-def create_json(data):
-    try:
-        response = { 'latlon' : [data[1],data[2]], 'time' : data[3]}
-        return response
-    except TypeError:
-        return { 'locations' : 'empty'}
+API_ADDR = os.environ['API_URL'] if 'API_URL' in os.environ else "https://api.naesheim.com/v1/getlatest"
 
 def get_latest():
     r = requests.get(API_ADDR)
-    return r.json()
+    try:
+        response = { 'latlon' :[r[1],r[2]], 'time' : r[3]}
+    except TypeError:
+        return {'locations' : 'empty'}
+
+    return json.dumps(response)
 
 @app.route("/")
 def base():
-    pos = get_latest()
-    jsonified_data = create_json(pos)
-    print(jsonified_data)
-    return render_template('index.html', data=pos)
+    return render_template('index.html', data=get_latest())
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
