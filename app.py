31638e21deac29db8c7ce07393362ab5a23732e6
naesheim <hg.nesheim@gmail.com>
2019-09-23 12:51:07 +0200

    refactor python

--- app.py
+++ app.py
@@ -8,17 +8,12 @@ app = Flask(__name__, static_url_path='/static')
 API_ADDR = os.environ['API_URL'] if 'API_URL' in os.environ else "https://api.naesheim.com/v1/getlatest"
 
 def get_latest():
-    r = requests.get(API_ADDR)
-    try:
-        response = { 'latlon' :[r[1],r[2]], 'time' : r[3]}
-    except TypeError:
-        return {'locations' : 'empty'}
-
-    return json.dumps(response)
+    response = requests.get(API_ADDR)
+    return response.json()
 
 @app.route("/")
 def base():
     return render_template('index.html', data=get_latest())
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=8000)
